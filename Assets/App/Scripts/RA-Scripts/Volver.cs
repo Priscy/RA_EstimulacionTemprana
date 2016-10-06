b8fefb9b5871567a0856e67caa29fe6736965816
myllis wang <myllisxd@gmail.com>
2016-10-05 19:31:05 -0500

    Volver agregado

---
+++ Assets/App/Scripts/RA-Scripts/Volver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Volver : MonoBehaviour {
+	public UIButton volverButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable () {
+		EventDelegate.Add (volverButton.onClick,volver);
+	}
+
+	void OnDisable(){
+		
+	}
+
+	void volver(){
+		Application.LoadLevel ("00.Menu_CambiosEder");
+	}
+}
